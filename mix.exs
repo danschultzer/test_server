2f7528c1a8e75f5c05232c947b6ff4733d9964a8
Richard Ash <r.ash8347@gmail.com>
2026-03-13 16:24:46 -0700

    Add TestServer.SSH
    Co-Authored-By: Dan Schultzer <1254724+danschultzer@users.noreply.github.com>

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule TestServer.MixProject do
 
   def application do
     [
-      extra_applications: [:logger, :crypto, :public_key, :inets],
+      extra_applications: [:logger, :crypto, :public_key, :ssh, :inets],
       mod: {TestServer.Application, []}
     ]
   end
